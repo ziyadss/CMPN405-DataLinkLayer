ccc2b1cd7f48d8188624154994d6c773e55b2397
Ziyad <ziyad.ss@hotmail.com>
2021-12-11 02:36:18 +0200

    Added static modifier and Node::toBytes

--- src/Node.cc
+++ src/Node.cc
@@ -3,54 +3,65 @@
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program.  If not, see http://www.gnu.org/licenses/.
-// 
+//
 
 #include "Node.h"
 
-namespace cmpn405_datalinklayer {
-
-Define_Module(Node);
-
-uint8_t Node::CRC(const std::string &payload, const uint8_t generator)
+namespace cmpn405_datalinklayer
 {
-    uint8_t rem = 0;
 
-    for (const uint8_t &byte : payload)
+    Define_Module(Node);
+
+    uint8_t Node::CRC(const std::string &payload, const uint8_t generator)
     {
-        rem ^= byte;
-        for (int i = 0; i < 8; i++)
-            rem = rem << 1 ^ (rem >> 7) * generator;
+        uint8_t rem = 0;
+
+        for (const uint8_t &byte : payload)
+        {
+            rem ^= byte;
+            for (int i = 0; i < 8; i++)
+                rem = rem << 1 ^ (rem >> 7) * generator;
+        }
+
+        return rem;
     }
 
-    return rem;
-}
+    std::vector<std::bitset<8>> Node::toBits(const std::string &payload)
+    {
+        std::vector<std::bitset<8>> bits(payload.size());
 
-std::vector<std::bitset<8>> Node::toBits(const std::string &payload)
-{
-    std::vector<std::bitset<8>> bits(payload.size());
+        for (int i = 0; i < payload.size(); i++)
+            bits[i] = std::bitset<8>(payload[i]);
 
-    for (int i = 0; i < payload.size(); i++)
-        bits[i] = std::bitset<8>(payload[i]);
+        return bits;
+    }
 
-    return bits;
-}
+    std::string Node::toBytes(const std::vector<std::bitset<8>> &bits)
+    {
+        std::string bytes(bits.size(), ' ');
 
-void Node::initialize()
-{
-    // TODO - Generated method body
-}
+        for (int i = 0; i < bits.size(); i++)
+            bytes[i] = bits[i].to_ulong();
 
-void Node::handleMessage(cMessage *msg)
-{
-    // TODO - Generated method body
-}
+        return bytes;
+    }
+
+    void Node::initialize()
+    {
+        // TODO - Generated method body
+    }
+
+    void Node::handleMessage(cMessage *msg)
+    {
+        // TODO - Generated method body
+    }
 
 } //namespace
--- src/Node.h
+++ src/Node.h
@@ -3,15 +3,15 @@
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program.  If not, see http://www.gnu.org/licenses/.
-// 
+//
 
 #ifndef __CMPN405_DATALINKLAYER_NODE_H_
 #define __CMPN405_DATALINKLAYER_NODE_H_
@@ -21,19 +21,22 @@
 
 using namespace omnetpp;
 
-namespace cmpn405_datalinklayer {
+namespace cmpn405_datalinklayer
+{
 
-/**
+  /**
  * TODO - Generated class
  */
-class Node : public cSimpleModule
-{
-    uint8_t CRC(const std::string &payload, const uint8_t generator);
-    std::vector<std::bitset<8>> toBits(const std::string &payload);
+  class Node : public cSimpleModule
+  {
+    static uint8_t CRC(const std::string &payload, const uint8_t generator);
+    static std::vector<std::bitset<8>> toBits(const std::string &payload);
+    static std::string toBytes(const std::vector<std::bitset<8>> &bits);
+
   protected:
     virtual void initialize();
     virtual void handleMessage(cMessage *msg);
-};
+  };
 
 } //namespace
 
